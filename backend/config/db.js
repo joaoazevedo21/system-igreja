ac9b4cbc34b1cf1028f813dd4e112e5733310dab
Joao Azevedo <joaomauricior80@hotmail.com>
2026-04-22 16:21:43 +0100

    add db certo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 
-// 🔥 SE ESTIVER EM PRODUÇÃO (Render / Neon)
+// 🔥 PRODUÇÃO (Render / Neon)
 const pool = process.env.DATABASE_URL
   ? new Pool({
       connectionString: process.env.DATABASE_URL,
@@ -9,7 +9,7 @@ const pool = process.env.DATABASE_URL
       }
     })
 
-  // 🔥 SENÃO (LOCAL - teu código atual mantido)
+  // 🔥 LOCAL (teu computador)
   : new Pool({
       user: "postgres",
       host: "localhost",
